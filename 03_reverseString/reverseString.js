ce20ea5bbd18aed309d58a3206c1db210a2e79fe
Veanty <plmulticraftpl@gmail.com>
2022-06-05 17:06:03 +0200

    Complete exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  const textToChar = text.split("");
+  const arrayReversed = textToChar.reverse();
+  const reversed = arrayReversed.join("");
+  return reversed;
 };
 
 // Do not edit below this line
