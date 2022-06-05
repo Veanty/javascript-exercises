de9a281e6ee3a09843cd9e863416e71293cb5804
Veanty <plmulticraftpl@gmail.com>
2022-06-05 16:36:41 +0200

    Complete exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+  if (number < 0) {
+    return "ERROR";
+  } else {
+    return text.repeat(number);
+  }
 };
 
 // Do not edit below this line
