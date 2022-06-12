b9c1af515045b79c8c8d39aa4e2f7f13a22815fc
Veanty <plmulticraftpl@gmail.com>
2022-06-12 16:54:40 +0200

    Complete exercise number 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,53 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+// const theOldest = people.sort((a, b) => {
+//   const aAge = a.yearOfDeath - a.yearOfBirth;
+//   const bAge = b.yearOfDeath - b.yearOfBirth;
+//   if (aAge > bAge) {
+//     return -1;
+//   } else {
+//     return 1;
+//   }
+// });
+
+// console.table(theOldest);
+
+const findTheOldest = function (array) {
+  let currentYear = new Date().getFullYear();
+  const theOldest = array
+    .map((person) => {
+      let age = person.yearOfDeath - person.yearOfBirth;
+      if (isNaN(age)) {
+        age = currentYear - person.yearOfBirth;
+      }
+      return {
+        name: person.name,
+        age: age,
+      };
+    })
+    .sort((a, b) => {
+      if (a.age > b.age) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  return theOldest[0];
 };
 
 // Do not edit below this line
