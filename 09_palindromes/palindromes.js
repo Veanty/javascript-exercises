7cb18b25f34b8164e347a57d24fd45c1166fed2c
Veanty <plmulticraftpl@gmail.com>
2022-06-12 14:28:28 +0200

    Complete exercise number 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = (text) => {
+  if (!text) {
+    return true;
+  }
+  const cleanText = text
+    .toLowerCase()
+    .replace(/[.,/#!$%^&*;:{}=-_`~() ]/g, "")
+    .replace(/s{2,}/g, " ");
+  const charArray = cleanText.split("");
+  return charArray.join() === charArray.reverse().join();
 };
 
 // Do not edit below this line
