c0736cde3a9cb2f83342f80a6375afe5b17ab159
Veanty <plmulticraftpl@gmail.com>
2022-06-12 15:18:30 +0200

    Complete exercise number 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = (array) => {
+  const titles = array.map((item) => item.title);
+  return titles;
 };
 
 // Do not edit below this line
